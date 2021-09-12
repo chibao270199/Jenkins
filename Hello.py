8d0164f29113099761527e90af1cae5655d381a3
chibao270199 <nguyenchibao27011999@gmail.com>
2021-09-12 17:56:20 +0700

    09-12-21

---
+++ Hello.py
@@ -0,0 +1,12 @@
+a='*'*2
+b='*'*6
+c='*'*8
+d=' '
+e=' '*2
+f=' '*3
+print(a,e,a,d,b,d,a,e,d,e,a,e,e,d,c)
+print(a,e,a,d,a,f,d,a,e,d,e,a,e,e,d,a,e,a)
+print(c,d,b,d,a,e,d,e,a,e,e,d,a,e,a)
+print(a,e,a,d,a,f,d,a,e,d,e,a,e,e,d,a,e,a)
+print(a,e,a,d,b,d,c,d,c,d,c)
+print("Nguyễn Chí Bảo-14MMC")
